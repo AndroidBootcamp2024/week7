9bd275569b8935235ce39a0028a26f1cbe0e0e5b
jalfaro <jalfaro.pineda@gmail.com>
2024-03-09 23:47:31 -0600

    Homeworks first steps

--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoScreen.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/CountryInfoScreen.kt
@@ -8,9 +8,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
+import com.kodeco.android.countryinfo.flow.Flows
 import com.kodeco.android.countryinfo.models.Country
 import com.kodeco.android.countryinfo.network.CountryService
 import com.kodeco.android.countryinfo.sample.sampleCountries
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Response
 
 sealed class CountryInfoState {
@@ -39,25 +47,24 @@ fun CountryInfoScreen(
 
     if (state == CountryInfoState.Loading) {
         LaunchedEffect(key1 = "fetch-countries") {
-            // TODO: Move this to a private method
-            //  and have the method return a Flow<CountryInfoState>
-            //  NOTE: This method can utilize the flow { } builder.
-            //  Don't forget you can also remove the try/catch and catch directly from the flow!
-            state = try {
-                val countriesResponse = service.getAllCountries()
-
-                if (countriesResponse.isSuccessful) {
-                    CountryInfoState.Success(countriesResponse.body()!!)
-                } else {
-                    CountryInfoState.Error(Throwable("Request failed: ${countriesResponse.message()}"))
-                }
-            } catch (exception: Exception) {
-                CountryInfoState.Error(exception)
-            }
+            getCountryInfoFlow(service).collect { state = it }
         }
+
     }
 }
 
+private fun getCountryInfoFlow(service: CountryService): Flow<CountryInfoState> = flow {
+            Flows.resetCounterFlow()
+            delay(5000)
+            val countriesResponse = service.getAllCountries()
+            if (countriesResponse.isSuccessful) {
+                emit(CountryInfoState.Success(countriesResponse.body()!!))
+            } else {
+                emit(CountryInfoState.Error(Throwable("Request failed: ${countriesResponse.message()}")))
+            }
+    }.catch {
+        CountryInfoState.Error(it)
+    }
 @Preview
 @Composable
 fun CountryInfoScreenPreview() {
--- app/src/main/java/com/kodeco/android/countryinfo/ui/components/Loading.kt
+++ app/src/main/java/com/kodeco/android/countryinfo/ui/components/Loading.kt
@@ -3,21 +3,41 @@ package com.kodeco.android.countryinfo.ui.components
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Text
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kodeco.android.countryinfo.flow.Flows
 
 @Composable
 fun Loading() {
+    var counterState: Int by remember { mutableIntStateOf(Flows.counterFlow.value) }
+    LaunchedEffect(Unit) {
+        Flows.counterFlow.collect {
+            counterState = it
+        }
+    }
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center,
     ) {
-
-        // TODO: Show the current Flows.counterFlow value here in Text composable
+        Text(
+            text="Loading... App up time $counterState",
+            modifier = Modifier
+                .padding(0.dp, 0.dp, 0.dp, 20.dp)
+        )
         CircularProgressIndicator()
     }
 }
